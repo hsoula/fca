b730d749afb6d30a98dc130ed1ce9a98600d7832
/Ed <hsoula@gmail.com>
2017-04-20 15:44:17 +0200

    adding a functional test function

--- test_fca.py
+++ test_fca.py
@@ -9,32 +9,32 @@ import numpy as np
 import numpy.random as rnd
 from fca import functional_clustering_algorithm
 
-rnd.seed(2)
-n_trains = 10
 
-# create two group - one periodical with noise and one random
+def test_functional_clustering():
+    rnd.seed(2)
+    n_trains = 10
 
-n_group1 = 3
-n_group2 = n_trains - n_group1
+    # create two group - one periodical with noise and one random
 
+    n_group1 = 3
+    n_group2 = n_trains - n_group1
 
-train_list = []
-tmax = 1000.0
-freq1 = 1.0
-period = 3.5
+    train_list = []
+    tmax = 1000.0
+    freq1 = 1.0
+    period = 3.5
 
-print "creatin spikes train"
-for i in range(n_group1):
-    train = np.arange(1.0 * rnd.random(), tmax, period)
-    train_list.append(train)
-for i in range(n_group2):
-    train = np.cumsum([-np.log(rnd.random())/freq1 for i in range(int(tmax*freq1))])
-    train = train[train < tmax]
-    train_list.append(train)
-for i in range(n_group1):
-    train = np.arange(0.0 * rnd.random(), tmax, period)
-    train_list.append(train)
-
-merge_history, current_cluster = functional_clustering_algorithm(train_list, 300, .10)
-print merge_history
-print current_cluster
+    print "creating spikes train"
+    for i in range(n_group1):
+        train = np.arange(1.0 * rnd.random(), tmax, period)
+        train_list.append(train)
+    for i in range(n_group2):
+        train = np.cumsum([-np.log(rnd.random())/freq1 for i in range(int(tmax*freq1))])
+        train = train[train < tmax]
+        train_list.append(train)
+    for i in range(n_group1):
+        train = np.arange(0.0 * rnd.random(), tmax, period)
+        train_list.append(train)
+    print "starting clustering"
+    merge_history, current_cluster = functional_clustering_algorithm(train_list, 300, .10)
+    return train_list, merge_history, current_cluster
